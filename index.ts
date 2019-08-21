38988b54e4e2b1b27c69b27172118ef534e2696a
Morsby <sigurd.morsby@gmail.com>
2019-08-21 11:05:08 +0200

    more refactoring

--- index.ts
+++ index.ts
@@ -1,20 +1,21 @@
+import colors = require("colors/safe");
 import * as fs from "fs";
 import indexFlows from "./utils/indexFlows";
 import { validateFlow } from "./utils/validation/isValidFlow";
 import { returnError } from "./utils/errors/errorHandler";
 import { Flow } from "./interfaces/Flow";
 import { getPreviousFlowIds } from "./utils/getPreviousFlows";
-const inputFile = JSON.parse(fs.readFileSync("flow-example.json").toString());
 
-const test = async () => {
+const inputFile = JSON.parse(fs.readFileSync(process.argv[2]).toString());
+
+const test = () => {
   const prevFlows = getPreviousFlowIds();
-  let index = await indexFlows(inputFile);
+  let index = indexFlows(inputFile);
   console.log(index);
   //index = index.filter(f => f.questions > 50);
   index.forEach(({ title, questions, activityId }, i) => {
     let string = `${i}. ${title} (${questions} questions)`;
-    if (prevFlows.includes(activityId)) console.log("already exists");
-    console.log(string);
+    if (prevFlows.includes(activityId)) console.log(colors.red(string));
   });
 
   const selectedFlows = [0];
--- utils/indexFlows.ts
+++ utils/indexFlows.ts
@@ -1,5 +1,5 @@
+import * as fs from "fs";
 import { Flow } from "../interfaces/Flow";
-
 import { calculateQuestionCount } from "./validation/calculateQuestionCount";
 
 interface FlowIndex {
@@ -10,14 +10,16 @@ interface FlowIndex {
 
 const indexFlows = (flows: Flow[]): FlowIndex[] => {
   let index: FlowIndex[] = [];
-
+  let ids: number[] = [];
   flows.map(flow => {
+    ids.push(flow.activityId);
     index.push({
       title: flow.title,
       questions: calculateQuestionCount(flow),
       activityId: flow.activityId
     });
   });
+  fs.writeFileSync("prevFlows.json", JSON.stringify(ids));
   return index;
 };
 
